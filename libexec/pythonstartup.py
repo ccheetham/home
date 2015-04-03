5498040ce95d4c80552cbc48fcd8ad344773b546
Chris Cheetham <chris@cheetham.com>
2015-04-03 10:03:42 -0400

    reorg

--- libexec/pythonstartup.py
+++ libexec/pythonstartup.py
@@ -1,27 +1,31 @@
-import atexit
-import os
-import readline
-import socket
+# ===========================================================================
+# startup actions upon entering Python's interactive interpreter
+# ===========================================================================
 
-# =============================
-# start: per-host history files
-#
+def config_cli_history():
+    '''
+    configure commandline interpreter history:
+      * define a per-host history file
+      * if needed, create directory to house history files
+      * if available, read in previous history file
+      * save commandline history to history file upon exit
+    '''
+    import atexit, os, readline, socket
+    history_home = os.path.expanduser("~/.python_history")
+    if not os.path.exists(history_home):
+        os.makedirs(history_home)
+    history_path = os.path.join(history_home, socket.gethostname())
+    if os.path.exists(history_path):
+        readline.read_history_file(history_path)
+    def save_history(path=history_path):
+        import readline
+        readline.write_history_file(path)
+        del readline
+    atexit.register(save_history)
+    del atexit, os, readline, socket
 
-history_dirpath = os.path.expanduser("~/.python_history")
-if not os.path.exists(history_dirpath):
-    os.makedirs(history_dirpath)
-history_path = os.path.join(history_dirpath, socket.gethostname())
-
-def save_history(history_path=history_path):
-    import readline
-    readline.write_history_file(history_path)
-atexit.register(save_history)
-
-if os.path.exists(history_path):
-    readline.read_history_file(history_path)
-
-del atexit, os, readline, history_path, save_history
-
-#
-# end: per-host history files
-# ===========================
+if __name__ == '__main__':
+    config_cli_history()
+    # pre-import frequently used modules
+    import re
+    import os
