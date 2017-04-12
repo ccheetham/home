af804759deeed03d4eeb779a3884af9a76a05152
Chris Cheetham <chris@cheetham.com>
2017-04-12 08:18:19 -0400

    convert xinitrc to xinitrc.d

---
+++ .xinitrc.d/keyboard.sh
@@ -0,0 +1,6 @@
+if [[ $(uname -s) !=  Darwin ]]
+then
+    xset r rate 250 30
+fi
+
+# vim: ft=sh
---
+++ .xinitrc.d/vmware.sh
@@ -0,0 +1,6 @@
+if type vmware-user-suid-wrapper >/dev/null 2>&1
+then
+    vmware-user-suid-wrapper
+fi
+
+# vim: ft=sh
