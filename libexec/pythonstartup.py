83c77afb9673f7851b956a92465f4b8bd5d23dfa
Chris Cheetham <chris@cheetham.com>
2017-12-06 15:46:53 -0800

    import more mods, add some dummy vars

--- libexec/pythonstartup.py
+++ libexec/pythonstartup.py
@@ -2,9 +2,32 @@
 # startup actions upon entering Python's interactive interpreter
 # ===========================================================================
 
-import logging
+# commonly needed modules
 import platform
 import re
 import os
+path = os.path
 import shutil
+import subprocess
 import sys
+
+# command history
+try:
+    import readline, atexit
+    histdir = os.path.join(os.environ['ME_CACHE_DIR'], 'python')
+    try:
+        os.makedirs(histdir)
+    except FileExistsError:
+        pass
+    histfile = os.path.join(histdir, 'history')
+    if os.path.exists(histfile):
+        readline.read_history_file(histfile)
+    atexit.register(readline.write_history_file, histfile)
+    del readline, atexit
+except ImportError:
+    pass
+
+# some dummy vars
+s = 'this is a sample string'
+a = ['a', 'b', 'c']
+d = {'a':'A', 'b':'B', 'c':'C'}
