3187dc30b94edf6a1eaefc6bc924a27042b919a9
Chris Cheetham <chris@cheetham.com>
2021-05-12 08:12:38 -0400

    update path to tcserver qa

--- etc/profile.d/90tcserver.ps1
+++ etc/profile.d/90tcserver.ps1
@@ -2,7 +2,7 @@
 # tc Server
 # =============================================================================
 
-$TcsQa="$Env:UserProfile\src\appsuite\tcs-tc-server-qa"
+$TcsQa="$Env:UserProfile\src\github.com\appsuite\tcs-tc-server-qa"
 
 function tcd {
   cd "$TcsQa"
