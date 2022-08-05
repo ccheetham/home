4a76bb681f23aae7fea663959db93282bd16f85e
Chris Cheetham <chris@cheetham.com>
2022-08-05 12:43:36 -0400

    add support for java on windows

---
+++ etc/profile.d/30java.ps1
@@ -0,0 +1,9 @@
+$Env:JDK6_HOME="C:\Program Files\Java\jdk1.6.0_45"
+$Env:JDK7_HOME="C:\Program Files\Java\jdk1.7.0_80"
+$Env:JDK8_HOME="C:\Program Files\BellSoft\LibericaJDK-8"
+$Env:JDK11_HOME="C:\Program Files\BellSoft\LibericaJDK-11"
+$Env:JDK17_HOME="C:\Program Files\BellSoft\LibericaJDK-17"
+$Env:JDK18_HOME="C:\Program Files\BellSoft\LibericaJDK-18"
+
+$Env:JDK_HOME="$Env:JDK11_HOME"
+$Env:JAVA_HOME="$Env:JDK_HOME"
