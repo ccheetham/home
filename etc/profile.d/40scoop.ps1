ebab2585799bb9ec95f074e8d285967da36daaea
Chris Cheetham <chris@cheetham.com>
2022-08-17 11:14:36 -0400

    convenience to update scoop and apps

--- etc/profile.d/40scoop.ps1
+++ etc/profile.d/40scoop.ps1
@@ -1,2 +1,11 @@
 $Env:SCOOP="$Env:ME_PLATFORM_TOOL_DIR\scoop"
 $Env:SCOOP_CACHE="$Env:XDG_CACHE_HOME\scoop"
+
+Set-Alias sc scoop
+
+function scsync
+{
+  sc update
+  sc update --all
+  sc cleanup --all
+}
