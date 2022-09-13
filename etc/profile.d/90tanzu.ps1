bab016177bebc3c3e7658741108d92c9535dab90
Chris Cheetham <chris@cheetham.com>
2022-09-13 11:26:03 -0400

    update tanzu helpers

--- etc/profile.d/90tanzu.ps1
+++ etc/profile.d/90tanzu.ps1
@@ -5,7 +5,7 @@
 $Tanzu_Repos="$Env:ME_SRC_DIR/github.com/vmware-tanzu"
 
 function vt { cd "$Tanzu_Repos" }
-function vtvs { cd "$Tanzu_Repos/tanzu-developer-tools-for-visual-studio" }
 function vtc { cd "$Tanzu_Repos/tanzu-toolkit-for-vscode" }
-function vtij { cd "$Tanzu_Repos/tanzu-toolkit-for-intellij" }
-function vtls { cd "$Tanzu_Repos/tanzu-toolkit-language-server" }
+function vtj { cd "$Tanzu_Repos/tanzu-toolkit-for-intellij" }
+function vtl { cd "$Tanzu_Repos/tanzu-toolkit-language-server" }
+function vts { cd "$Tanzu_Repos/tanzu-developer-tools-for-visual-studio" }
