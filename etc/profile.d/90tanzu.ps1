d6a9713476f0aeb73e9c70caac6c8eeba72a509c
Chris Cheetham <chris@cheetham.com>
2022-09-30 06:35:19 -0400

    update tanzu helpers

--- etc/profile.d/90tanzu.ps1
+++ etc/profile.d/90tanzu.ps1
@@ -5,9 +5,10 @@
 $Env:TANZU_REPOS = "$Env:HUB_SOURCES\vmware-tanzu"
 
 function vt { cd "$Env:TANZU_REPOS" }
-function vtc { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-vscode" }
-function vtj { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-intellij" }
-function vtl { cd "$Env:TANZU_REPOS\tanzu-toolkit-language-server" }
-function vts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
+function dtc { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-vscode" }
+function dtj { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-intellij" }
+function dtl { cd "$Env:TANZU_REPOS\tanzu-toolkit-language-server" }
+function dts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
+function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
 
-function tt { cd "$Env:HUB_SOURCES/steeltoeoss-incubator/tanzu-thingies" }
+function tt { cd "$Env:HUB_SOURCES\steeltoeoss-incubator/tanzu-thingies" }
