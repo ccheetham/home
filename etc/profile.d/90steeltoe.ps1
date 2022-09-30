815257cc0f855650ac82db86007328cf35abd2dd
Chris Cheetham <chris@cheetham.com>
2022-09-30 06:48:07 -0400

    update steeltoe helpers

--- etc/profile.d/90steeltoe.ps1
+++ etc/profile.d/90steeltoe.ps1
@@ -2,17 +2,17 @@
 # Steeltoe
 # =============================================================================
 
-$Steeltoe_Repos="$Env:ME_SRC_DIR/github.com/SteeltoeOSS"
+$Steeltoe_Repos="$Env:ME_SRC_DIR\github.com\SteeltoeOSS"
 
 function st { cd "$Steeltoe_Repos" }
-function stf { cd "$Steeltoe_Repos/Steeltoe" }
-function sts { cd "$Steeltoe_Repos/Samples" }
-function std { cd "$Steeltoe_Repos/Dockerfiles" }
-function stt { cd "$Steeltoe_Repos/Tooling" }
-function sti { cd "$Steeltoe_Repos/InitializrWorkspace" }
-function stiw { cd "$Steeltoe_Repos/InitializrWorkspace/InitializrWeb" }
-function stic { cd "$Steeltoe_Repos/InitializrWorkspace/InitializrConfig" }
-function stik { cd "$Steeltoe_Repos/InitializrWorkspace/InitializrConfigServer" }
-function stis { cd "$Steeltoe_Repos/InitializrWorkspace/InitializrService" }
-function ntt { cd "$Steeltoe_Repos/InitializrWorkspace/NetCoreToolTemplates" }
-function nts { cd "$Steeltoe_Repos/InitializrWorkspace/NetCoreToolService" }
+function stl { cd "$Steeltoe_Repos\Steeltoe" }
+function sts { cd "$Steeltoe_Repos\Samples" }
+function std { cd "$Steeltoe_Repos\Dockerfiles" }
+function stt { cd "$Steeltoe_Repos\Tooling" }
+function sti { cd "$Steeltoe_Repos\InitializrWorkspace" }
+function stiw { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrWeb" }
+function stic { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrConfig" }
+function stik { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrConfigServer" }
+function stis { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrService" }
+function ntt { cd "$Steeltoe_Repos\InitializrWorkspace\NetCoreToolTemplates" }
+function nts { cd "$Steeltoe_Repos\InitializrWorkspace\NetCoreToolService" }
