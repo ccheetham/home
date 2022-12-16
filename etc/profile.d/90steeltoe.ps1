63e3254d7b40bd4c202ca9b4413c26feddbf7f15
Chris Cheetham <chris@cheetham.com>
2022-12-16 10:46:20 -0500

    update steeltoe helpers

--- etc/profile.d/90steeltoe.ps1
+++ etc/profile.d/90steeltoe.ps1
@@ -3,6 +3,7 @@
 # =============================================================================
 
 $Steeltoe_Repos="$Env:ME_SRC_DIR\github.com\SteeltoeOSS"
+$Steeltoe_Incubator_Repos="$Env:ME_SRC_DIR\github.com\steeltoeoss-incubator"
 
 function st { cd "$Steeltoe_Repos" }
 function stl { cd "$Steeltoe_Repos\Steeltoe" }
@@ -16,3 +17,7 @@ function stik { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrConfigServer" 
 function stis { cd "$Steeltoe_Repos\InitializrWorkspace\InitializrService" }
 function ntt { cd "$Steeltoe_Repos\InitializrWorkspace\NetCoreToolTemplates" }
 function nts { cd "$Steeltoe_Repos\InitializrWorkspace\NetCoreToolService" }
+
+function stinc { cd "$Steeltoe_Incubator_Repos" }
+function tt { cd "$Steeltoe_Incubator_Repos\tanzu-thingies" }
+function dtg { cd "$Steeltoe_Incubator_Repos\dotnet-tap-gitops" }
