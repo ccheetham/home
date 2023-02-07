0ec72a799429342fca24c035158bcc31b23ea5a0
Chris Cheetham <chris@cheetham.com>
2023-02-07 15:24:42 -0500

    add java to path

--- etc/profile.d/30java.ps1
+++ etc/profile.d/30java.ps1
@@ -1,9 +1,11 @@
-$Env:JDK6_HOME="C:\Program Files\Java\jdk1.6.0_45"
-$Env:JDK7_HOME="C:\Program Files\Java\jdk1.7.0_80"
-$Env:JDK8_HOME="C:\Program Files\BellSoft\LibericaJDK-8"
-$Env:JDK11_HOME="C:\Program Files\BellSoft\LibericaJDK-11"
-$Env:JDK17_HOME="C:\Program Files\BellSoft\LibericaJDK-17"
-$Env:JDK18_HOME="C:\Program Files\BellSoft\LibericaJDK-18"
+$Env:JDK19_HOME="C:\Tools\Java\jdk-19"
+$Env:JDK17_HOME="C:\Tools\Java\jdk-17"
+$Env:JDK11_HOME="C:\Tools\Java\jdk-11"
+$Env:JDK7_HOME="C:\Tools\Java\jdk-7"
+$Env:JDK8_HOME="C:\Tools\Java\jdk-8"
+$Env:JDK6_HOME="C:\Tools\Java\jdk-6"
 
 $Env:JDK_HOME="$Env:JDK11_HOME"
 $Env:JAVA_HOME="$Env:JDK_HOME"
+
+$ME_PATH = "$ME_PATH;$Env:JDK_HOME\bin"
