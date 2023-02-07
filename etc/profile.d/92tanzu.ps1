d310d15f5469a4862520df7efca9b8a4b419d087
Chris Cheetham <chris@cheetham.com>
2023-02-07 07:07:23 -0500

    add support for ME_PATH on windows

--- etc/profile.d/92tanzu.ps1
+++ etc/profile.d/92tanzu.ps1
@@ -10,3 +10,6 @@ function dtj { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-intellij" }
 function dtl { cd "$Env:TANZU_REPOS\tanzu-toolkit-language-server" }
 function dts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
 function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
+
+
+$ME_PATH = "$ME_PATH;$STEELTOE_INCUBATOR_REPOS\tanzu-thingies\local\bin"
