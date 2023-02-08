05f602abc6b14e24e451af779b483063f2995510
Chris Cheetham <chris@cheetham.com>
2023-02-08 08:20:15 -0500

    fix aliases

--- etc/profile.d/92tanzu.ps1
+++ etc/profile.d/92tanzu.ps1
@@ -5,9 +5,9 @@
 $Env:TANZU_REPOS = "$Env:HUB_SOURCES\vmware-tanzu"
 
 function vt { cd "$Env:TANZU_REPOS" }
-function dtc { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-vscode" }
-function dtj { cd "$Env:TANZU_REPOS\tanzu-toolkit-for-intellij" }
-function dtl { cd "$Env:TANZU_REPOS\tanzu-toolkit-language-server" }
+function dtc { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-vscode" }
+function dtj { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-intellij" }
+function dtl { cd "$Env:TANZU_REPOS\tanzu-developer-tools-language-server" }
 function dts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
 function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
 
