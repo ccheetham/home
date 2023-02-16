4448685ee92e6f83e47e5085acdb7f57fdd7b5e0
Chris Cheetham <chris@cheetham.com>
2023-02-16 10:15:44 -0500

    convenience for tanzu workloads

--- etc/profile.d/92tanzu.ps1
+++ etc/profile.d/92tanzu.ps1
@@ -12,6 +12,7 @@ function dts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
 function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
 
 function dtslog { echo "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\language-server.log" }
-# function dtslog { tail "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\language-server.log" }
+
+function taw { tanzu apps workload $args }
 
 $ME_PATH = "$ME_PATH;$STEELTOE_INCUBATOR_REPOS\tanzu-thingies\local\bin"
