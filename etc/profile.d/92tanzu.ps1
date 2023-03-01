b5b836a0887811657cddc6a07f736d8cac04f630
Chris Cheetham <chris@cheetham.com>
2023-03-01 14:50:37 -0500

    use alias for t

--- etc/profile.d/92tanzu.ps1
+++ etc/profile.d/92tanzu.ps1
@@ -14,9 +14,9 @@ function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
 function dtslog { tail "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\tanzu-dev-tools.log" }
 function dtslslog { tail "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\tanzu-language-server.log" }
 
-function t { tanzu $args }
-function tme { tanzu --namespace ccheetham $args }
-function twg { tanzu -n ccheetham apps workload get $args }
-function twl { tanzu -n ccheetham apps workload list $args }
+set-alias t tanzu
+function tme { t --namespace ccheetham $args }
+function twg { t -n ccheetham apps workload get $args }
+function twl { t -n ccheetham apps workload list $args }
 
 $ME_PATH = "$ME_PATH;$STEELTOE_INCUBATOR_REPOS\tanzu-thingies\local\bin"
