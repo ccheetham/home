a3c9390db7ed08e5914162e924169ad7ef622afe
Chris Cheetham <chris@cheetham.com>
2023-03-27 09:51:59 -0400

    default to java 17

--- etc/profile.d/30java.ps1
+++ etc/profile.d/30java.ps1
@@ -1,11 +1,11 @@
-$Env:JDK19_HOME="C:\Tools\Java\jdk-19"
-$Env:JDK17_HOME="C:\Tools\Java\jdk-17"
-$Env:JDK11_HOME="C:\Tools\Java\jdk-11"
+$Env:JDK6_HOME="C:\Tools\Java\jdk-6"
 $Env:JDK7_HOME="C:\Tools\Java\jdk-7"
 $Env:JDK8_HOME="C:\Tools\Java\jdk-8"
-$Env:JDK6_HOME="C:\Tools\Java\jdk-6"
+$Env:JDK11_HOME="C:\Tools\Java\jdk-11"
+$Env:JDK17_HOME="C:\Tools\Java\jdk-17"
+$Env:JDKLATEST_HOME="C:\Tools\Java\jdk-latest"
 
-$Env:JDK_HOME="$Env:JDK11_HOME"
+$Env:JDK_HOME="$Env:JDK17_HOME"
 $Env:JAVA_HOME="$Env:JDK_HOME"
 
 $ME_PATH = "$ME_PATH;$Env:JDK_HOME\bin"
