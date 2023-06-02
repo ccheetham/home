7c9f8b6cb85ca2b33c95435648efbfd8715b8ed4
Chris Cheetham <chris@cheetham.com>
2023-06-02 06:30:02 -0400

    diable Gradle daemon on windows

--- etc/profile.d/50gradle.ps1
+++ etc/profile.d/50gradle.ps1
@@ -1,6 +1,6 @@
 $Env:GRADLE_HOME="$Env:ME_TOOL_DIR\opt\gradle"
 $Env:GRADLE_USER_HOME="$Env:ME\.gradle"
-$Env:GRADLE_OPTS="-Dorg.gradle.console=rich"
+$Env:GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=rich"
 
 Set-Alias gr .\gradlew.bat
 Function grr { gr --refresh-dependencies $Args }
