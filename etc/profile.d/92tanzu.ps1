9252d9c61f81f368ab4a5c4690ed81b694352c28
Chris Cheetham <chris@cheetham.com>
2023-07-26 09:16:49 -0400

    log aliases

--- etc/profile.d/92tanzu.ps1
+++ etc/profile.d/92tanzu.ps1
@@ -3,6 +3,8 @@
 # =============================================================================
 
 $Env:TANZU_REPOS = "$Env:HUB_SOURCES\vmware-tanzu"
+$Env:TANZU_DT_LOG = "$Env:ME_LOG_DIR\tanzu_dt.log"
+$Env:TANZU_LS_LOG = "$Env:ME_LOG_DIR\tanzu_ls.log"
 
 function vtr { cd "$Env:TANZU_REPOS" }
 function dtc { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-vscode" }
@@ -11,12 +13,12 @@ function dtl { cd "$Env:TANZU_REPOS\tanzu-developer-tools-language-server" }
 function dts { cd "$Env:TANZU_REPOS\tanzu-developer-tools-for-visual-studio" }
 function aas { cd "$Env:TANZU_REPOS\application-accelerator-samples" }
 
-function dtslog { tail "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\tanzu-dev-tools.log" }
-function dtslslog { tail "$Env:LOCALAPPDATA\Microsoft\VisualStudio\17.0_84b9b701Exp\Extensions\VMware\Tanzu Developer Tools\0.0.0\tanzu-language-server.log" }
-
-function twl { tanzu apps workload list $args }
-function twg { tanzu apps workload get $args }
-function twd { tanzu apps workload delete $args }
-function twt { tanzu apps workload tail --timestamp $args }
+function dtslog { tail "$Env:TANZU_DT_LOG" 3 }
+function dtslslog { tail "$Env:TANZU_LS_LOG" 3 }
 
 $ME_PATH = "$ME_PATH;$STEELTOE_INCUBATOR_REPOS\tanzu-thingies\local\bin"
+
+function twl { tanzu apps workload list "$Args" }
+function twg { tanzu apps workload get "$Args" }
+function twd { tanzu apps workload delete "$Args" }
+function twt { tanzu apps workload tail --timestamp "$Args" }
