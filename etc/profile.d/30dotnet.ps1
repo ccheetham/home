db4f1e80c434b8aa1ab949be64eb2b3b4f543dac
Chris Cheetham <chris@cheetham.com>
2023-09-25 09:05:31 -0400

    add dotnet tools to path

--- etc/profile.d/30dotnet.ps1
+++ etc/profile.d/30dotnet.ps1
@@ -1,3 +1,7 @@
 $Env:DOTNET_ROOT="$Env:ME_PLATFORM_TOOL_DIR/dotnet"
 $Env:DOTNET_CLI_TELEMETRY_OPTOUT="1"
+
+$ME_PATH = "$ME_PATH;$Env:ME\.dotnet\tools"
+$ME_PATH = "$ME_PATH;$Env:DOTNET_ROOT"
+
 Set-Alias dn dotnet
