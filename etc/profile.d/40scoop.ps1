99462e114dc978ec6d9c5fbc70356eaa04e4a7e2
Chris Cheetham <chris@cheetham.com>
2023-09-25 08:55:55 -0400

    add scoop to path

--- etc/profile.d/40scoop.ps1
+++ etc/profile.d/40scoop.ps1
@@ -1,6 +1,8 @@
 $Env:SCOOP="$Env:ME_TOOL_DIR\scoop"
 $Env:SCOOP_CACHE="$Env:XDG_CACHE_HOME\scoop"
 
+$ME_PATH = "$ME_PATH;$Env:SCOOP\shims"
+
 function scsync {
     scoop update
     scoop update --all
