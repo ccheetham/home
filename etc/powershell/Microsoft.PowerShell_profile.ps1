796cf5f173d01da3f13f65b49d66f43cc20e9e14
Chris Cheetham <chris@cheetham.com>
2023-11-13 11:08:58 -0500

    add windows local support

--- etc/powershell/Microsoft.PowerShell_profile.ps1
+++ etc/powershell/Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,19 @@ if (Test-Path env:HOME) {
 }
 
 
-"Sourcing $($profileDir)"
-foreach($file in Get-ChildItem "$profileDir" -Filter "*.ps1") {
+"Sourcing profile directory $profileDir"
+
+if (Test-Path "$profileDir\local.pre.ps1") {
+    # "Sourcing $profileDir\local.pre.ps1"
+    . "$profileDir\local.pre.ps1"
+}
+
+foreach ($file in Get-ChildItem "$profileDir" | Where-Object {$_ -Match '\d\d.*.ps1'}) {
+    # "Sourcing $file"
     . $file.FullName
+}
+
+if (Test-Path "$profileDir\local.post.ps1") {
+    # "Sourcing $profileDir\local.post.ps1"
+    . "$profileDir\local.post.ps1"
 }
