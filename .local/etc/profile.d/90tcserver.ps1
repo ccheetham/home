750d547a14be320fcf0426ee263ea8b35d54b693
Chris Cheetham <chris@cheetham.com>
2026-06-01 13:39:25 -0400

    remove stale configs

--- .local/etc/profile.d/90tcserver.ps1
+++
@@ -1,3 +0,0 @@
-$Env:TCS_QA="$Env:ME_CODE_DIR\gitlab.eng.vmware.com\vmware-tc-server\tcs-tc-server-qa"
-
-function tcd { cd "${Env:TCS_QA}" }
--- .local/etc/profile.d/92cloudfoundry.ps1
+++
@@ -1,8 +0,0 @@
-# =============================================================================
-# Cloud Foundry
-# =============================================================================
-
-$Env:CF_REPOS = "$Env:HUB_SOURCES\pivotal-cf"
-
-function cfr { cd "$Env:CF_REPOS" }
-function kbp { cd "$Env:CF_REPOS\kerberos-buildpack" }
